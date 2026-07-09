5f7eb7c03aff5e1d0534f1c26c255ab7ea0325c1
Sunny | Engineering Student | Computer Science Enthusiast <137380955+Sunny-Saurya@users.noreply.github.com>
2026-07-09 11:05:12 +0530

    Time: 0 ms (100%), Space: 8.7 MB (19.4%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -9,7 +9,11 @@ public:
         // return solveMemo(n, dp);
 
         // tabulation
-        return solveTab(n);
+        // return solveTab(n);
+
+        // sapce
+
+        return solveSpace(n);
     }
 
     int solve(int n){
@@ -38,4 +42,21 @@ public:
 
         return dp[n];
     }
+
+    // Space
+
+     int solveSpace(int n){
+        vector<int>dp(n+1, 0);
+        int pp = 1;
+        int p = 1;
+        
+        for(int i = 2; i <= n; i++){
+            int curr = pp+p;
+            pp = p;
+            p = curr;
+        }
+
+        return p;
+    }
+
 };
