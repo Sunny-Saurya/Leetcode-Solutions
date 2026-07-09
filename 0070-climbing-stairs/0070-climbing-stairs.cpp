662d842779f14b90cc7145e1365886f5a33f5475
Sunny | Engineering Student | Computer Science Enthusiast <137380955+Sunny-Saurya@users.noreply.github.com>
2026-07-09 11:03:07 +0530

    Time: 0 ms (100%), Space: 8.5 MB (51.94%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,20 +1,41 @@
 class Solution {
 public:
-    int climbStairs(int n) {
-        // base case
-        // if(n == 0 || n == 1) return 1;
+    int climbStairs(int n) {  
+        //recursion  
+        // return solve(n);
+        // vector<int>dp(n+1, -1);
 
-        // this solution gives TLE
-        // return climbStairs(n-1) + climbStairs(n-2);
-        vector<int>dp(n, -1);
-        return solveMemo(n, dp);
+        // memo
+        // return solveMemo(n, dp);
+
+        // tabulation
+        return solveTab(n);
     }
 
-    int solveMemo(int n, vector<int>&dp){
-
-        // base cases
+    int solve(int n){
         if(n == 0 || n == 1) return 1;
-        if(dp[n-1] != -1) return dp[n-1];
-        return dp[n-1] = solveMemo(n-1, dp) + solveMemo(n-2, dp);
+
+        return solve(n-1) + solve(n-2);
+    }
+
+    // memoization 
+    int solveMemo(int n, vector<int>&dp){
+        
+        if(n == 0 || n == 1) return 1;
+        if(dp[n] !=  -1) return dp[n];
+        return dp[n] =  solveMemo(n-1, dp) + solveMemo(n-2, dp);
+    }
+
+    // Tabulation 
+
+    int solveTab(int n){
+        vector<int>dp(n+1, 0);
+        dp[0] = dp[1] = 1;
+        
+        for(int i = 2; i <= n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
     }
 };
