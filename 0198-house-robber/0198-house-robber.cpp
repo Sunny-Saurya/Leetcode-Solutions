082d24097523faa0a754f01195a45bfa8442b7eb
Sunny | Engineering Student | Computer Science Enthusiast <137380955+Sunny-Saurya@users.noreply.github.com>
2026-07-09 11:46:14 +0530

    Time: 0 ms (100%), Space: 10.8 MB (23.23%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -3,19 +3,37 @@ public:
     int rob(vector<int>& nums) {
         int n = nums.size()-1;
         vector<int>dp(n+1, -1);
-        return solve(n,nums, dp);
+        return solveMemo(n,nums, dp);
     }
 
-    int solve(int idx, vector<int>&nums, vector<int>&dp){
+    // Memoizatino
+    int solveMemo(int idx, vector<int>&nums, vector<int>&dp){
         if(idx == 0 ) return nums[0];
         if(idx < 0) return 0;
 
         if(dp[idx] != -1) return dp[idx];
 
-        int take = nums[idx] + solve(idx-2, nums, dp);
-        int noteTake = 0 + solve(idx-1, nums, dp);
+        int take = nums[idx] + solveMemo(idx-2, nums, dp);
+        int noteTake = 0 + solveMemo(idx-1, nums, dp);
 
         return dp[idx] = max(take, noteTake);
 
+    }
+
+    // Tabulation
+    int solveTab(int idx, vector<int>&nums){
+        vector<int>dp(nums.size(), 0);
+
+        dp[0] = nums[0];
+        if(idx < 0) return 0;
+
+        for(int idx = 1; idx <= nums.size(); idx++){
+            int take = nums[idx] + dp[idx-2];
+            int noteTake = 0 + dp[idx-1];
+
+            dp[idx] = max(take, noteTake);
+        }
+
+        return dp[nums.size()-1];
     }
 };
