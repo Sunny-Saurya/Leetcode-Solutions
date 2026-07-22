d248e09a23ecd8e816036208041dd0e4ffb7c883
Sunny | Engineering Student | Computer Science Enthusiast <137380955+Sunny-Saurya@users.noreply.github.com>
2026-07-22 22:35:45 +0530

    Time: 0 ms (100%), Space: 27.4 MB (17.45%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,39 +1,33 @@
 class Solution {
-    vector<int>prefix, suffix;
-
-    void prefixMax(vector<int>&height){
-        int n = height.size();
-        prefix.assign(n, 0);
-        prefix[0] = height[0];
-        for(int i = 1; i < n; i++){
-            prefix[i] = max(prefix[i-1], height[i]);
-        }
-    }
-
-    void suffixMax(vector<int>&height){
-        int n = height.size();
-        suffix.assign(n, 0);
-        suffix[n-1] = height[n-1];
-        for(int i = n-2; i >= 0; i--){
-            suffix[i] = max(suffix[i+1], height[i]);
-        }
-    }
 public:
     int trap(vector<int>& height) {
         int n = height.size();
-        if(n == 0) return 0;
+        vector<int>leftMax(n), rightMax(n);
+        prefixMax(height, leftMax);
+        suffixMax(height, rightMax);
 
-        prefixMax(height);
-        suffixMax(height);
-
-        int totalWaterTrapped = 0;
-        for(int i = 0; i < n; i++){
-            if(prefix[i] == 0|| suffix[i] == 0) continue;
-            int waterCap = min(prefix[i], suffix[i]);
-            if(waterCap > height[i]){
-                totalWaterTrapped += waterCap - height[i];
-            }
+        int total = 0;
+        for(int i = 0; i < height.size(); i++){
+            if(leftMax[i] > height[i] && rightMax[i] > height[i])
+                total += min(leftMax[i],  rightMax[i]) - height[i];
         }
-        return totalWaterTrapped;
+
+        return total;
+    }
+
+    void prefixMax(vector<int>&arr, vector<int>&leftMax){
+        leftMax[0] = arr[0];
+
+        for(int i = 1; i < arr.size(); i++){
+            leftMax[i] = max(leftMax[i-1], arr[i]);
+        }
+    }
+
+    void suffixMax(vector<int>&arr, vector<int>&rightMax){
+        rightMax[arr.size()-1] = arr[arr.size()-1];
+
+        for(int i = arr.size()-2; i >= 0; i--){
+            rightMax[i] = max(rightMax[i+1], arr[i]);
+        }
     }
 };
